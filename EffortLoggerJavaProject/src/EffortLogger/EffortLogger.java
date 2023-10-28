575438638c9bbdfa90871bc0e3ce240ef2a4b3e3
JKoehle <118227709+JKoehle@users.noreply.github.com>
2023-10-28 14:24:41 -0700

    Update EffortLogger.java

--- EffortLoggerJavaProject/src/EffortLogger/EffortLogger.java
+++ EffortLoggerJavaProject/src/EffortLogger/EffortLogger.java
@@ -8,7 +8,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
- 
+
+//Code written by Justin Koehle
 public class EffortLogger extends Application {
     protected String username;
     public static void main(String[] args) {
@@ -16,9 +17,17 @@ public class EffortLogger extends Application {
     }
     
     public void start(Stage primaryStage) {
+     
+        //Set the FXML loader to load the main console interface which will be loaded after accepting a username
+        Parent console = FXMLLoader.load(getClass().getResource("Effort Log Console.fxml"));
+       	Scene consoleScene = new Scene(console);
+
+        //Sets the window's title
         primaryStage.setTitle("EffortLogger Login Interface");
         
         BorderPane root = new BorderPane();
+
+        //Text entry field for the username
         TextField loginPrompt = new TextField("Enter your username");
      
         //Login button instantiation and event handling
