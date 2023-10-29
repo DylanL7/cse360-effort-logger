e5d1740e7edcd2597c254f876b531fa27bd1786f
Jia-Jui <149220887+Jia-Jui@users.noreply.github.com>
2023-10-29 08:26:45 +0800

    Update CSVInterface.java

--- EffortLoggerJavaProject/src/EffortLogger/CSVInterface.java
+++ EffortLoggerJavaProject/src/EffortLogger/CSVInterface.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,18 +13,31 @@ public class CSVInterface {
 	
 	static class LogEntry {
 		public String task_name;
-		public String start_time;
-		public String end_time;
+		public LocalDateTime start_time;
+		public LocalDateTime end_time;
 		public String user_name;
 		
 		// Create a class constructor for the Main class
-		public LogEntry(String task_name, String start_time, String end_time, String user_name) {
+		public LogEntry(String task_name, LocalDateTime start_time, LocalDateTime end_time, String user_name) {
 			this.task_name = task_name;
 			this.start_time = start_time;
 			this.end_time = end_time;
 			this.user_name = user_name;
 		}
+		
+		public String getFormattedStartTime() {
+	        return start_time.toString();
+	    }
+		public String getFormattedEndTime() {
+	        if (end_time != null) {
+	            return end_time.toString();
+	        } else {
+	            return "";
+	        }
+	    }
 	}
+	
+	
 	
 	@SuppressWarnings("finally")
 	static void WriteLoggerData(LogEntry entry) {
@@ -51,10 +66,12 @@ public class CSVInterface {
 			while (myReader.hasNextLine()) {
 				String data = myReader.nextLine();
 				//System.out.println(data);
-				String[] raw_dat = data.split("[,]", 0);
-				
+				String[] raw_dat = data.split(",", -1);
+				LocalDateTime startTime = LocalDateTime.parse(raw_dat[1]);
+                LocalDateTime endTime = raw_dat[2].isEmpty() ? null : LocalDateTime.parse(raw_dat[2]);
+
 				// this is the same order it came in
-				LogEntry read_entry = new LogEntry(raw_dat[0], raw_dat[1], raw_dat[2], raw_dat[3]);
+                LogEntry read_entry = new LogEntry(raw_dat[0], startTime, endTime, raw_dat[3]);
 				return_array.add(read_entry);
 			}
 			myReader.close();
@@ -67,6 +84,35 @@ public class CSVInterface {
 		return return_array;
 	}
 	
+	
+	public static LogEntry startTask(String taskName, String userName) {
+        LocalDateTime startTime = LocalDateTime.now();
+        return new LogEntry(taskName, startTime, null, userName);
+    }
+
+    public static void endTask(LogEntry entry) {
+        entry.end_time = LocalDateTime.now();
+    }
+
+    public static void main(String[] args) {
+        LogEntry le = startTask("TaskA", "User1");
+        try {
+            Thread.sleep(2000); // Wait for 2 seconds to simulate task duration
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        endTask(le);
+        WriteLoggerData(le);
+
+        ArrayList<LogEntry> read_entries = ReadLoggedData();
+        for (LogEntry entry : read_entries) {
+            System.out.println("Data read: " + entry.task_name + " " + entry.getFormattedStartTime() + " " + 
+                               entry.getFormattedEndTime() + " " + entry.user_name);
+        }
+        System.out.println("Done");
+    }
+}
+
 	/*
 	public static void main(String[] args) {
 		
@@ -85,6 +131,6 @@ public class CSVInterface {
 		System.out.println("Done");
     }*/
 
-}
+
 
     
