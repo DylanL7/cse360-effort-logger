8a24e929e2a2d2ca98747dd11c23b207cf2f8721
DylanL7 <dllarso4@asu.edu>
2023-11-11 13:27:18 -0700

    updated csv for planning poker

--- EffortLoggerJavaProject/src/EffortLogger/CSVInterface.java
+++ EffortLoggerJavaProject/src/EffortLogger/CSVInterface.java
@@ -4,8 +4,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -15,51 +13,66 @@ public class CSVInterface {
 	// make a class to hold inputs and outputs from the CSV, makes it easier to read
 	static class LogEntry {
 		public String task_name;
-		public LocalDateTime start_time;
-		public LocalDateTime end_time;
+		public int actual_story_points_hours;
+		public int estimated_story_points;
 		public String user_name;
 		
 		// Create a class constructor for the Main class
-		public LogEntry(String task_name, LocalDateTime start_time, LocalDateTime end_time, String user_name) {
+		public LogEntry(String task_name, String user_name, int estimated_story_points, int actual_story_points_hours) {
 			this.task_name = task_name;
-			this.start_time = start_time;
-			this.end_time = end_time;
+			this.actual_story_points_hours = actual_story_points_hours;
+			this.estimated_story_points = estimated_story_points;
 			this.user_name = user_name;
 		}
 		
-		public String getFormattedStartTime() {
-	        return start_time.toString();
-	    }
-		public String getFormattedEndTime() {
-	        if (end_time != null) {
-	            return end_time.toString();
-	        } else {
-	            return "";
-	        }
-	    }
-		
-		// validate each string property of this class for the CSV file - make sure nothing has commas in it
-		public boolean validatePropertiesForCSV() {
-	        if (task_name.indexOf(',') != -1 || user_name.indexOf(',') != -1) {
-	        	return false;
-	        }
-	        
-	        return true; // everything was OK
-	    }
 	}
 	
 	
-	// verify and write input data to the CSV file
+	/// Call from the console WHEN PLANNING POKER is clicked
 	@SuppressWarnings("finally")
-	static void WriteLoggerData(LogEntry entry) {
+	static void WriteInitialTaskName(String task_name, String user_name) {
 		
-		// validate inputs here
-		if (!entry.validatePropertiesForCSV()) {
+		// validate input for the CSV (make sure there are no commas)
+		if (task_name.indexOf(',') != -1 || user_name.indexOf(',') != -1) {
 			System.out.println("Invalid inputs for CSV!");
 			return;
 		}
 		
-		String new_entry = entry.task_name + "," + entry.start_time + "," + entry.end_time  + "," + entry.user_name + "\n";
+		String new_entry = task_name + "," + user_name + ","; // start new line, don't end it
+		
+		try {
+			// write to the local file
+			FileWriter fw = new FileWriter("localCSV.csv", true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(new_entry);
+			bw.close();
+		} finally {
+			return;
+		}
+	}
+	
+	/// To be called from the planning poker screen - estimated_points will be the input in that screen
+	@SuppressWarnings("finally")
+	static void WriteEstimatedPoints(int estimated_points) {
+		
+		String new_entry = estimated_points + ","; // start new line, don't end it - still need actual points
+		
+		try {
+			// write to the local file
+			FileWriter fw = new FileWriter("localCSV.csv", true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(new_entry);
+			bw.close();
+		} finally {
+			return;
+		}
+	}
+	
+	/// To be called from the console AFTER planning poker
+	@SuppressWarnings("finally")
+	static void WriteActualPoints(int actual_points) {
+		
+		String new_entry = actual_points + "\n"; // end the line here - last entry
 		
 		try {
 			// write to the local file
@@ -85,11 +98,9 @@ public class CSVInterface {
 				String data = myReader.nextLine();
 				//System.out.println(data);
 				String[] raw_dat = data.split(",", -1);
-				LocalDateTime startTime = LocalDateTime.parse(raw_dat[1]);
-                LocalDateTime endTime = raw_dat[2].isEmpty() ? null : LocalDateTime.parse(raw_dat[2]);
-
+				
 				// this is the same order it came in
-                LogEntry read_entry = new LogEntry(raw_dat[0], startTime, endTime, raw_dat[3]);
+                LogEntry read_entry = new LogEntry(raw_dat[0], raw_dat[1], Integer.parseInt(raw_dat[2]), Integer.parseInt(raw_dat[3]));
 				return_array.add(read_entry);
 			}
 			myReader.close();
@@ -101,54 +112,24 @@ public class CSVInterface {
 		
 		return return_array;
 	}
-	
-	// keep track of start and end times
-	public static LogEntry startTask(String taskName, String userName) {
-        LocalDateTime startTime = LocalDateTime.now();
-        return new LogEntry(taskName, startTime, null, userName);
-    }
-    public static void endTask(LogEntry entry) {
-        entry.end_time = LocalDateTime.now();
-    }
 
     // public class to test this interface with
     public static void main(String[] args) {
-        LogEntry le = startTask("TaskA", "User1");
-        LogEntry le2 = startTask("TaskB", "User,2");
-        try {
-            Thread.sleep(2000); // Wait for 2 seconds to simulate task duration
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        endTask(le);
-        endTask(le2);
-        WriteLoggerData(le);
-        WriteLoggerData(le2);
-
-        ArrayList<LogEntry> read_entries = ReadLoggedData();
+    	
+    	WriteInitialTaskName("task1", "user1");
+    	WriteEstimatedPoints(5);
+    	WriteActualPoints(3);
+    	
+    	WriteInitialTaskName("task2", "user2");
+    	WriteEstimatedPoints(3);
+    	WriteActualPoints(6);
+    	
+    	ArrayList<LogEntry> read_entries = ReadLoggedData();
         for (LogEntry entry : read_entries) {
-            System.out.println("Data read: " + entry.task_name + " " + entry.getFormattedStartTime() + " " + 
-                               entry.getFormattedEndTime() + " " + entry.user_name);
+            System.out.println("Data read: " + entry.task_name + " " + entry.estimated_story_points + " " + 
+                               entry.actual_story_points_hours + " " + entry.user_name);
         }
         System.out.println("Done");
     }
 }
-
-	/*
-	public static void main(String[] args) {
-		
-		LogEntry le = new LogEntry("a","b","c","d");
-		WriteLoggerData(le);
-		
-		LogEntry le2 = new LogEntry("1","2","3","4");
-		WriteLoggerData(le2);
-		
-		ArrayList<LogEntry> read_entries = ReadLoggedData();
-		for (int i = 0; i < read_entries.size(); i++) {
-			System.out.println("Data read: " + read_entries.get(i).task_name + " " + read_entries.get(i).start_time + " " +
-								read_entries.get(i).end_time + " " + read_entries.get(i).user_name);
-		}
-		
-		System.out.println("Done");
-    }*/
 
